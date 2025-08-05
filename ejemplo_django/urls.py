f0f20dc631f9507a5794ad2bb3f12ac4fe5a793c
Cristian Pinto <crisma8988@gmail.com>
2025-08-05 14:27:58 -0300

    cambiamos el estilo a un estilo BS

--- ejemplo_django/urls.py
+++ ejemplo_django/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -23,3 +25,6 @@ urlpatterns = [
     path('', include('mi_primer_app.urls')),
     # path('hola-mundo/', saludo),
 ]
+
+urlpatterns += static (settings.STATIC_URL,
+                        document_root=settings.STATIC_ROOT)
